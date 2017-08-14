19dacf940645ba2c6bc9de0c10ba17ef78fe0b18
davidelena <2747142992qq.com>
2017-08-14 11:43:29 +0800

    complex object store in redis kv

--- simple-dal/src/test/java/org/simple/dal/AppTest.java
+++ simple-dal/src/test/java/org/simple/dal/AppTest.java
@@ -1,20 +1,32 @@
 package org.simple.dal;
 
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.lang3.RandomUtils;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.simple.util.JUnit4ClassRunner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.ContextConfiguration;
 
+import com.alibaba.fastjson.JSON;
+import com.david.dal.model.MemberInfo;
+
 @RunWith(JUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath*:spring/applicationContext-dal.xml" })
 public class AppTest {
-	
+
+	private static final Logger log = LoggerFactory.getLogger(AppTest.class);
+
 	@Autowired
 	private StringRedisTemplate redisTemplate;
-	
+
 	@Test
 	public void stringOpsGetAndSet() {
 		String key = "member";
@@ -25,7 +37,48 @@ public class AppTest {
 		redisTemplate.delete(key);
 		String value2 = redisTemplate.opsForValue().get(key);
 		Assert.assertNull(value2);
-		
+	}
+
+	private MemberInfo getMemberInfo() {
+		MemberInfo memberInfo = new MemberInfo();
+		memberInfo.setAge(RandomUtils.nextInt(0, 50));
+		memberInfo.setEmail(RandomStringUtils.randomNumeric(9) + "@qq.com");
+		memberInfo.setId(RandomUtils.nextInt(1, 1000));
+		memberInfo.setMemberNo(RandomStringUtils.randomAlphanumeric(32));
+		memberInfo.setMobile("135" + RandomStringUtils.randomNumeric(8));
+		memberInfo.setRealname("daivdreal_" + RandomStringUtils.randomNumeric(6));
+		memberInfo.setUsername("daivduser_" + RandomStringUtils.randomNumeric(6));
+		memberInfo.setStatus(1);
+		memberInfo.setCreateTime(new Date());
+		memberInfo.setModifyTime(new Date());
+		return memberInfo;
+	}
+
+	@Test
+	public void stringOpsGetAndSetObject() {
+		log.info("redist str key start");
+		String key = "member_info";
+		MemberInfo memberInfo = getMemberInfo();
+		redisTemplate.opsForValue().set(key, JSON.toJSONString(memberInfo));
+		String value = redisTemplate.opsForValue().get(key);
+		MemberInfo resultMemberInfo = JSON.parseObject(value, MemberInfo.class);
+		System.out.println(value);
+		Assert.assertEquals(memberInfo.getMemberNo(), resultMemberInfo.getMemberNo());
+		System.out.println("set value for ttl");
+		String newkey = "member_info_new";
+		redisTemplate.opsForValue().set(newkey, JSON.toJSONString(memberInfo), 1, TimeUnit.SECONDS);
+		String ttlValue = redisTemplate.opsForValue().get(newkey);
+		System.out.println("ttlValue=" + ttlValue);
+		Assert.assertNotNull(ttlValue);
+		try {
+			TimeUnit.SECONDS.sleep(1);
+		} catch (InterruptedException ex) {
+			log.error(ex.getMessage(), ex);
+		}
+		String ttlValueExpired = redisTemplate.opsForValue().get(newkey);
+		System.out.println("ttlValueExpired=" + ttlValueExpired);
+		Assert.assertNull(ttlValueExpired);
+		log.info("redist str key end");
 	}
 
 }
