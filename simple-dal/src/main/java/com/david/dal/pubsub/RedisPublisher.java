0e1781f797178f9bd7d78f9fcda56d6dced658a5
daviddai <dlwdavid@hotmail.com>
2017-08-17 01:08:12 +0800

    redis.properties check

--- simple-dal/src/main/java/com/david/dal/pubsub/RedisPublisher.java
+++ simple-dal/src/main/java/com/david/dal/pubsub/RedisPublisher.java
@@ -3,10 +3,8 @@ package com.david.dal.pubsub;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.david.test.dal.AppTest;
-
 public class RedisPublisher {
 	
-	private static final Logger log = LoggerFactory.getLogger(AppTest.class);
+	private static final Logger log = LoggerFactory.getLogger(RedisPublisher.class);
 	
 }
