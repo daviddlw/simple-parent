fb1db9d7095ab5ede1ea8da97fa4246851c00f22
davidelena <2747142992qq.com>
2017-08-17 14:44:17 +0800

    spring-redis-data pub or sub

--- simple-dal/src/main/java/com/david/dal/pubsub/RedisMessageListener.java
+++ simple-dal/src/main/java/com/david/dal/pubsub/RedisMessageListener.java
@@ -1,5 +1,10 @@
 package com.david.dal.pubsub;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.connection.MessageListener;
@@ -14,10 +19,21 @@ public class RedisMessageListener implements MessageListener {
 	@Override
 	public void onMessage(Message message, byte[] pattern) {
 		RedisSerializer<?> serializer = redisTemplate.getValueSerializer();
-		Object channel = serializer.deserialize(message.getChannel());
+		Object obj = message.getChannel();
+//		System.out.println(obj.getClass());
+//		Object channel = serializer.deserialize(message.getChannel());
+//		System.out.println(String.valueOf(channel));
 		Object body = serializer.deserialize(message.getBody());
-		System.out.println("subject=" + channel);
-		System.out.println("content=" + String.valueOf(body));
+		if (body.getClass() == String.class) {
+			System.out.println(String.valueOf(body));
+		} else if (body.getClass().isArray()) {
+			System.out.println(Arrays.toString((Object[]) body));
+		} else if (body instanceof List<?> || body instanceof Map<?, ?>) {
+			System.out.println(body);
+		} else {
+			System.out.println(ToStringBuilder.reflectionToString(body));
+		}
+
 	}
 
 	public RedisTemplate<String, String> getRedisTemplate() {
---
+++ simple-dal/src/test/java/com/david/test/dal/RedisPublisherTest.java
@@ -0,0 +1,35 @@
+package com.david.test.dal;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.lang3.RandomUtils;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.simple.util.JUnit4ClassRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+
+import com.david.dal.model.User;
+import com.david.dal.pubsub.RedisMessageDAO;
+
+@RunWith(JUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath*:spring/applicationContext-dal.xml" })
+public class RedisPublisherTest {
+
+	@Autowired
+	private RedisMessageDAO redisMessageDAO;
+
+	private static final String TOPIC = "userTopic";
+
+	@Test
+	public void redisPublishMessageTest() {
+		// common javabean
+		User user = new User(RandomUtils.nextInt(1, 100), RandomStringUtils.randomAlphabetic(6));
+		redisMessageDAO.sendMessage(TOPIC, user);
+
+		// common string
+		String message = "Hello, world_" + System.currentTimeMillis();
+		redisMessageDAO.sendMessage(TOPIC, message);
+
+	}
+
+}
